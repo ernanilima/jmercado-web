d6f904c7466c471d81b922ca4b2649196a12e200
ernanilima <ernani.tecc@gmail.com>
2022-12-06 17:10:45 -0300

    aplicacao backend criada

---
+++ jmercado-backend/src/main/java/br/com/ernanilima/jmercado/JMercadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ernanilima.jmercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JMercadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JMercadoApplication.class, args);
+    }
+
+}
---
+++ jmercado-backend/src/test/java/br/com/ernanilima/jmercado/JMercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ernanilima.jmercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JMercadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
